0a512b686fe365006681d5656a315d8b41014cb1
AlexGomesDS <Alexsantogomes@gmail.com>
2018-01-02 15:37:38 +0000

    [UPD] - Minor bug fixes

--- install.py
+++ install.py
@@ -11,24 +11,27 @@ from zipfile import ZipFile
 import re, os, sys
 
 framework_repo_zip_url = 'https://github.com/AlexGomesDS/Kaggle_project_framework/archive/master.zip'
+repo_path = '.'
 
 
 def main():
-	repo_path = '.'
 	if len(sys.argv) < 2:
 		print('Correct usage: python install.py repository_name [repository path]')
 		return -1
 	elif len(sys.argv) == 3:
 		repo_path = sys.argv[2]
-	
 	repo_name = sys.argv[1]
+	full_repo_path = repo_path + '/' + repo_name
+	
 	with urlopen(framework_repo_zip_url) as zipresp:
 		with ZipFile(BytesIO(zipresp.read())) as zfile:
-			zfile.extractall(members = [i for i in zfile.namelist() if re.match('Kaggle_project_framework-master/.*', i)])
+			files_to_extract = zfile.namelist()
+			files_to_extract.remove('Kaggle_project_framework-master/install.py')
+			zfile.extractall(path = repo_path, members = files_to_extract)
 		
-	os.rename('Kaggle_project_framework-master', repo_name)
+	os.rename(repo_path+'/Kaggle_project_framework-master', full_repo_path)
 	
-	with open(repo_name+'\\Readme.md','w') as readme_file:
+	with open(full_repo_path + '/Readme.md','w') as readme_file:
 		readme_file.write('# Repository created from https://github.com/AlexGomesDS/Kaggle_project_framework\n\n' + repo_name + '\n (Insert description here)')
 	
 	print('Succes, repository copied to: {}\nDon\'t forget to add it in github'.format(repo_path + '\\' + repo_name))
