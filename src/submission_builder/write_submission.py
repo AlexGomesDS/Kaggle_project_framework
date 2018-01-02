95039c4835e26ae71f099c7fd331a59d634a766e
AlexGomesDS <Alexsantogomes@gmail.com>
2018-01-02 13:36:54 +0000

    [ADD] - Submission builder

---
+++ src/submission_builder/write_submission.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 24 21:16:10 2017
+
+@author: Joana Pinto
+"""
+
+import pandas as pd
+
+class SubmissionWriter:
+    
+    submissions_folder = '..\\ouput\\'
+
+    def build_submission(self, id, prediction, classes):
+        submission_DF = pd.concat([id,
+                                   pd.DataFrame(prediction, columns = classes)], 
+                       axis = 1)
+        return submission_DF
+    
+    def write_submission_to_csv(self, filename, id, predictions, classes):
+        self.build_submission(id, predictions, classes).to_csv(self.submissions_folder + filename, index = False)
+        
+#if __name__ == "__main__":
+    
