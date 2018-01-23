5ff80ad79338618776367708723254427bf8faa1
Alex <alexsantogomes@gmail.com>
2018-01-23 11:29:34 +0000

    [ADD] - Created configuration files to initialize travis CI

--- ci/test_house_price.py
+++ ci/test_house_price.py
@@ -13,10 +13,10 @@ import pandas as pd
 from house_price_prediction.framework_extensions.HousePricePred_mean_mode_preprocessor import Mean_Mode_Preprocessor
 from house_price_prediction.framework_extensions.HousePricePred_mean_price_label_encoder import Mean_Price_Preprocessor
 
-def test_hpp_preprocessor(preprocessor):
+def localtester_hpp_preprocessor(preprocessor):
     data_preprocessor = preprocessor(["SalePrice"])
-    train_dataframe = pd.read_csv('house_price_prediction/input/train.csv', index_col='Id')
-    test_dataframe = pd.read_csv('house_price_prediction/input/test.csv', index_col='Id')
+    train_dataframe = pd.read_csv('ci/house_price_prediction/input/train.csv', index_col='Id')
+    test_dataframe = pd.read_csv('ci/house_price_prediction/input/test.csv', index_col='Id')
   
     data_preprocessor.prepare(train_dataframe)
     X_train, y_train = data_preprocessor.cook_and_split(train_dataframe)
@@ -38,5 +38,6 @@ def test_hpp_preprocessor(preprocessor):
     assert(X_train.select_dtypes(exclude = [np.number]).shape[1] == 0)
     assert(X_test.select_dtypes(exclude = [np.number]).shape[1] == 0)
 
-test_hpp_preprocessor(Mean_Mode_Preprocessor)
-test_hpp_preprocessor(Mean_Price_Preprocessor)
+def test_hpp():
+	localtester_hpp_preprocessor(Mean_Mode_Preprocessor)
+	localtester_hpp_preprocessor(Mean_Price_Preprocessor)
